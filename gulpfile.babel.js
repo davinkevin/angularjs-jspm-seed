2deba812078d120becabe392472b768f435b7413
davinkevin <davin.kevin@gmail.com>
2015-10-21 10:35:21 +0200

    feat(release) Incorporate in gulp task the process of release

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,27 @@
 import gulp from "gulp";
 import sass from "gulp-sass"
+import jspm from 'jspm';
+import systemjs from 'systemjs';
+
 
 gulp.task("build-sass", () =>
     gulp.src(['./public/**/*.scss', '!./public/jspm_packages/**/*.scss'])
         .pipe(sass().on("error", sass.logError))
         .pipe(gulp.dest("./public"))
 );
+
+gulp.task('build', function(){
+  let builder = new jspm.Builder('public', 'public/system.config.js');
+  return builder.buildStatic('app', 'dist/app.js');
+    /*.then(function(output) {
+      console.log(output.source);
+    });*/
+
+
+  /*builder.config({baseURL : 'public'});
+  return builder.loadConfig('public/system.config.js')
+    .then(function() {
+        sourceMaps: true
+      });
+    });*/
+});
