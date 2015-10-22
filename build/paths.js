311b0bd1aab6c277a4c4290914ed5df222f6bc01
kendo5731 <aur.baumann@gmail.com>
2015-10-22 15:32:17 +0200

    feat(paths): move build paths to build.paths module

--- build/paths.js
+++ build/paths.js
@@ -3,12 +3,16 @@ import path from 'path';
 
 /* All constant and location of the application */
 
+const appName = 'app';
 const root = path.dirname(__dirname);
 
 export default {
   root : root,
+  appName: appName,
   systemConfigJs : `${root}/system.config.js`,
+  releaseDir: `${root}/dist`,
   app : {
-    entryPoint : 'public/app'
+    entryPoint : 'public/app',
+    name: appName
   }
 }
--- build/tasks/release.js
+++ build/tasks/release.js
@@ -14,31 +14,37 @@ gulp.task('build-jspm', function(cal){
   let builder = new Builder();
   builder.loadConfig(paths.systemConfigJs)
     .then(() => {
-      builder.buildStatic(paths.app.entryPoint, `dist/app.js`, {sourceMaps: true})
+      builder.buildStatic(paths.app.entryPoint, `${paths.releaseDir}/${paths.appName}.js`, {sourceMaps: true})
         .then(() => cal())
         .catch((ex) => cal(new Error(ex)));
     });
 });
 
 gulp.task('build-js', () => {
-  return gulp.src('dist/app.js')
+  return gulp.src(`${paths.releaseDir}/${paths.app.name}.js`)
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(rename({suffix : '.min'}))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest(paths.releaseDir))
 });
 
 gulp.task('build-css', () => {
-  return gulp.src('dist/app.css')
+  return gulp.src(`${paths.releaseDir}/${paths.app.name}.css`)
     .pipe(minifyCSS())
     .pipe(rename({suffix : '.min'}))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest(paths.releaseDir))
 });
 
 gulp.task('build-clean', (cal) => {
-  del(['./dist/app.css','./dist/app.css.map','./dist/app.js','./dist/app.js.map'], cal);
+  let targets = [
+      `${paths.releaseDir}/${paths.appName}.css`,
+      `${paths.releaseDir}/${paths.appName}.css.map`,
+      `${paths.releaseDir}/${paths.appName}.js`,
+      `${paths.releaseDir}/${paths.appName}.js.map`
+  ];
+  del(targets, cal);
 });
 
 gulp.task('build', (cal) => {
